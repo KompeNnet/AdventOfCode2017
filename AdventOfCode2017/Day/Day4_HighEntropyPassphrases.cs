4debeabb43d3d44400bef5723f6310c49cd83be8
KompeNnet <kompennet@gmail.com>
2017-12-04 22:47:59 +0300

    3n2 done & delegate added

--- AdventOfCode2017/Day/Day4_HighEntropyPassphrases.cs
+++ AdventOfCode2017/Day/Day4_HighEntropyPassphrases.cs
@@ -6,6 +6,7 @@ namespace AdventOfCode2017.Day
 {
     internal class Day4_HighEntropyPassphrases : IDay
     {
+        delegate string[] FormattingDelegate(string s);
         StreamReader fReader;
 
         internal Day4_HighEntropyPassphrases() { }
@@ -17,15 +18,24 @@ namespace AdventOfCode2017.Day
 
         public string FirstTask()
         {
+            return CountValid(SplitWords);
+        }
+
+        public string SecondTask()
+        {
+            return CountValid(SortLetter);
+        }
+
+        private string CountValid(FormattingDelegate func)
+        {
             int result = 0;
             string pass = fReader.ReadLine();
 
             while (pass != null)
             {
                 if (pass == "") continue;
-                string[] word = pass.Split(" ");
+                string[] word = func(pass);
                 bool valid = true;
-
                 foreach (string w in word)
                 {
                     if (word.Where(x => x.Equals(w)).Count() > 1)
@@ -41,9 +51,19 @@ namespace AdventOfCode2017.Day
             return result.ToString();
         }
 
-        public string SecondTask()
+        private string[] SplitWords(string pass)
         {
-            throw new NotImplementedException();
+            return pass.Split(" ");
+        }
+
+        private string[] SortLetter(string pass)
+        {
+            string[] result = SplitWords(pass);
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = String.Concat(result[i].OrderBy(x => x).ToArray());
+            }
+            return result;
         }
     }
 }
