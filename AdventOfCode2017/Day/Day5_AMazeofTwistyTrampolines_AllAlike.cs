5c5b71650b2e33e269b1c05e23133a81437afd1a
KompeNnet <kompennet@gmail.com>
2017-12-05 13:45:52 +0300

    5n2 done

--- AdventOfCode2017/Day/Day5_AMazeofTwistyTrampolines_AllAlike.cs
+++ AdventOfCode2017/Day/Day5_AMazeofTwistyTrampolines_AllAlike.cs
@@ -1,37 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 
 namespace AdventOfCode2017.Day
 {
     internal class Day5_AMazeofTwistyTrampolines_AllAlike : IDay
     {
+        delegate (int, int) CountDelegate(int i, int fromList);
         StreamReader fReader;
 
         internal Day5_AMazeofTwistyTrampolines_AllAlike() { }
 
+        public void SetFile(string path)
+        {
+            fReader = new StreamReader(path);
+        }
+
         public string FirstTask()
+        {
+            return GetCount(ByOnlyGrowing);
+        }
+
+        public string SecondTask()
+        {
+            return GetCount(By3InMiddle);
+        }
+
+        private string GetCount(CountDelegate func)
         {
             int result = 0;
             List<int> instructList = ParseInstruction();
-            int i = 0;            
+            int i = 0;
             while (i < instructList.Count && i >= 0)
             {
-                i += instructList[i]++;
+                int temp = i;
+                (i, instructList[temp]) = func(i, instructList[i]);
                 result++;
             }
             fReader.BaseStream.Position = 0;
             return result.ToString();
         }
 
-        public string SecondTask()
+        private (int, int) ByOnlyGrowing(int i, int fromList)
         {
-            throw new NotImplementedException();
+            return (i + fromList, ++fromList);
         }
 
-        public void SetFile(string path)
+        private (int, int) By3InMiddle(int i, int fromList)
         {
-            fReader = new StreamReader(path);
+            return (i + fromList, fromList < 3 ? ++fromList : --fromList);
         }
 
         private List<int> ParseInstruction()
